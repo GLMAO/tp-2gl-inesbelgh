7cb1a2c60cfeb99eefb5584e33398eff2fb604e3
ines <inesbelg@gmail.com>
2025-11-20 13:57:57 +0100

    Implémentation du pattern Observer

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -5,7 +5,22 @@ import java.util.List;
 
 public class GestionnaireEmploiDuTemps {
     private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>(); // <-- liste des observers
 
+    // Méthodes pour gérer les observateurs
+    public void attach(Observer o) {
+        observers.add(o);
+    }
+
+    public void detach(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer o : observers) {
+            o.update(message);
+        }
+    }
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
@@ -17,10 +32,11 @@ public class GestionnaireEmploiDuTemps {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+         notifyObservers("Cours modifié : " + message);
     }
 
-    public void setChangement(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
-    }
+   // public void setChangement(String string) {
+        // supprime Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
+   // }
 }
